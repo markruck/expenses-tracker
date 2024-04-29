0f8105051a7c90f87f48adc05b380a33364accea
Mark <mark@MBP14-10C4.fritz.box>
2024-04-29 18:30:31 +0200

    Change chart titles

--- components/income/incomeCharts.tsx
+++ components/income/incomeCharts.tsx
@@ -14,13 +14,13 @@ const IncomeCharts = () => {
     <div className="flex flex-1 flex-row space-between">
       <div style={{ width: '49.5%' }}>
         <ChartComponent data={chartData} chartType="PieChart" width="100%" height="300px" options={{
-          title: "Expenses by Category",
+          title: "Income by Category",
           is3D: true,
         }} className="chart" />
       </div>
       <div style={{ width: '49.5%' }}>
         <ChartComponent data={chartData} chartType="BarChart" width="100%" height="300px" options={{
-          title: "Expenses by Category",
+          title: "Income by Category",
           is3D: true,
         }} className="chart" />
       </div>
